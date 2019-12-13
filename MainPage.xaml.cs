904aee8bcd837d6647b6e9db6523d0ed80a45363
RabiaEkici <57702832+RabiaEkici@users.noreply.github.com>
2019-12-13 08:59:44 +0100

    rekenmachine finale
    final rekenmachine

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -41,11 +41,24 @@ namespace Rekenmachine
         public double result = 0.0;
         public char operation;
 
+        //punt knop
+        //in XAML wordt door de "onclick" attribute een functie gedraaid  met die naam.
+        //Door middel van de twee argumenten "Object sender, RouterEventArgs e" wordt de klik actie (event) geregistreert en gestuurd
         private void punt_click(object sender, RoutedEventArgs e)
         {
             textbox1.Text = "";
             input += ".";
             textbox1.Text += input;
+        }
+
+        private void hex_click(object sender, RoutedEventArgs e)
+        {
+            textbox1.Text = "";
+            operand1 = input;
+            input = "HEX";
+            operation = 'h';
+            textbox1.Text = input;
+
         }
 
         private void nul_click(object sender, RoutedEventArgs e)
@@ -73,13 +86,78 @@ namespace Rekenmachine
         {
 
             operand2 = input;
+
+           /* TryParse(String, Double) probeert te converteren van een getal naar het dubbele precisie-getal met drijvende-kommagetal.Een retourwaarde geeft aan of de conversie is geslaagd of mislukt. 
+            Als het is mislukt geeft hij geen error aan.*/
             double.TryParse(operand1, out getal1);
             double.TryParse(operand2, out getal2);
 
+            //hier berekent hij de resultaten van de operators die zijn aangegeven.
+            //if staat voor als, dus als het gelijk is aan. anders gaat hij naar de else-if.
+            //als het false is.
             if (operation == '+')
             {
                 result = getal1 + getal2;
                 textbox1.Text = result.ToString();
+            }
+
+            else if (operation == 'h')
+            {
+                if (textbox1.Text.Contains("."))
+                {
+                    textbox1.Text = ("Syntax Error");
+                }
+                else
+                {
+                    int index = Convert.ToInt32(operand1);
+                    if (index < 0)
+                    {
+                        index *= -1;
+                    }
+
+                    int result = index / 16;
+                    int rest = index % 16;
+                    String finalResult = "";
+
+                    if (result != 0)
+                    {
+                        finalResult = "" + result;
+                    }
+
+
+                    /* These if statements check if we have to add a letter to the hexadecimal number or not. */
+                    if (rest == 15)
+                    {
+                        finalResult += "F";
+                    }
+                    else if (rest == 14)
+                    {
+                        finalResult += "E";
+                    }
+                    else if (rest == 13)
+                    {
+                        finalResult += "D";
+                    }
+                    else if (rest == 12)
+                    {
+                        finalResult += "C";
+                    }
+                    else if (rest == 11)
+                    {
+                        finalResult += "B";
+                    }
+                    else if (rest == 10)
+                    {
+                        finalResult += "A";
+                    }
+                    else
+                    {
+                        finalResult += "" + rest;
+                    }
+
+                    textbox1.Text = finalResult;
+                    textbox2.Text = index + " =";
+                }
             }
 
             else if (operation == '-')
@@ -110,15 +188,19 @@ namespace Rekenmachine
                 textbox1.Text = result.ToString();
             }
 
+
+            // de wisselkoers van euro naar dollar is 1.11293 
+            // de wisselkoers van dollar naar euro is 0.898528
+            //Gebruik: Klik getal, klik dollar, klik = voor uitkomst
             else if (operation == '$')
             {
-                result = getal1 * 1.10092510737;
+                result = getal1 * 0.898528;
                 textbox1.Text = "€" + result.ToString();
             }
 
             else if (operation == '€')
             {
-                result = getal1 * 0.908633;
+                result = getal1 * 1.11293;
                 textbox1.Text = "$" + result.ToString();
             }
 
@@ -126,21 +208,22 @@ namespace Rekenmachine
             {
 
                 double negatiefgetal = getal1 * -1;
-                textbox1.Text = negatiefgetal.ToString();
+                textbox1.Text = "-" + negatiefgetal.ToString();
             }
         }
 
-        //ik snap dit zelf ook niet en weet niet hoe ik het moet maken
+        //(?)
         private void plusMin_click(object sender, RoutedEventArgs e)
         {
-            textbox1.Text = "-";
+
             operand1 = '-' + input;
             operation = 'a';
+            //string empty zorgt er voor dat input leeg word gemaakt
             input = string.Empty;
+
+            textbox1.Text = "-" + textbox1.Text;
         }
 
-        /* maakt eerst de textbox leeg (hoefde niet per se ziet er alleen iets beter uit, het gaat raar doen wanneer ik dit niet doe. 
-        De cijfers worden dan elke keer verdubbeld) */
         //input = 1 + het gene wat ervoor kwam
         //textbox.Text laat de nummers zien van input.
         //en dit word herhaald bij de nummers 0 t/m 9
@@ -167,10 +250,25 @@ namespace Rekenmachine
         }
 
         //laat de min zien in de textbox
-        //operand1 word input het dus alles op in de input en operand1 word alleen gebruikt voor operators
+        //operand1 word input en het slaat dus alles op in de input.
+        //operand1 word alleen gebruikt voor operators
         //de operation is '-' hier word het gekozen welke operator je wilt gebruiken.
-        //string.Empty heb ik eerder al uitgelegd en dat zorgt er voor dat input leeg word gemaakt
-        // en dit word dan gewoon een paar keer herhaald bij ander buttons
+        // en dit word dan gewoon een paar keer herhaald bij andere buttons.
+        private void min_click(object sender, RoutedEventArgs e)
+        {
+            textbox1.Text = "-";
+            operand1 = input;
+            operation = '-';
+            input = string.Empty;
+        }
+        //plus klik
+        private void plus_click(object sender, RoutedEventArgs e)
+        {
+            textbox1.Text = "+";
+            operand1 = input;
+            operation = '+';
+            input = string.Empty;
+        }
 
         private void vier_click(object sender, RoutedEventArgs e)
         {
@@ -214,15 +312,6 @@ namespace Rekenmachine
             input += "9";
             textbox1.Text += input;
         }
-
-        private void keer_click(object sender, RoutedEventArgs e)
-        {
-            textbox1.Text = "x";
-            operand1 = input;
-            operation = '*';
-            input = string.Empty;
-        }
-
         private void delen_click(object sender, RoutedEventArgs e)
         {
             textbox1.Text = "÷";
@@ -231,19 +320,11 @@ namespace Rekenmachine
             input = string.Empty;
         }
 
-        private void min_click(object sender, RoutedEventArgs e)
+        private void keer_click(object sender, RoutedEventArgs e)
         {
-            textbox1.Text = "-";
+            textbox1.Text = "x";
             operand1 = input;
-            operation = '-';
-            input = string.Empty;
-        }
-
-        private void plus_click(object sender, RoutedEventArgs e)
-        {
-            textbox1.Text = "+";
-            operand1 = input;
-            operation = '+';
+            operation = '*';
             input = string.Empty;
         }
 
@@ -262,9 +343,24 @@ namespace Rekenmachine
         //de eerste zorgt ervoor dat elke keer de text textbox tekst lengte met - 1 word gewist en de andere doet het bij de input 
         private void delete_click(object sender, RoutedEventArgs e)
         {
-            textbox1.Text = textbox1.Text.Remove(textbox1.Text.Length - 1);
-            input = input.Remove(input.Length - 1);
+            if (textbox1.Text != "" && CheckSymbol(textbox1.Text))
+            {
+                textbox1.Text = textbox1.Text.Remove(textbox1.Text.Length - 1);
+                if (input.Length > 0)
+                    input = input.Remove(input.Length - 1);
+            }
         }
+
+        //met != && str geef je aan dat als je op alles klikt dat het dan geen
+        //error geeft, geen bugs meer voorkomt.
+        private bool CheckSymbol(string str)
+        {
+            if (str != "-" && str != "+" && str != "x" && str != "€" && str != "$" && str != "%" && str != "." && str != "NaN" && str != "÷" && str[0] != '-')
+                return true;
+            else
+                return false;
+        }
+
 
         private void euro_click(object sender, RoutedEventArgs e)
         {
@@ -289,7 +385,30 @@ namespace Rekenmachine
             operation = '$';
             input = string.Empty;
         }
-        //binary to hexidecimal
+
+        //Collapsed geeft het besturingselement niet weer en reserveert niet de witruimte.
+        private void history_click(object sender, RoutedEventArgs e)
+        {
+            if (textbox2.Visibility == Visibility.Visible)
+            {
+                textbox2.Visibility = Visibility.Collapsed;
+            }
+            else if (textbox2.Visibility == Visibility.Collapsed)
+            {
+                textbox2.Visibility = Visibility.Visible;
+            }
+
+            //Een CNN bestaat uit meerdere lagen. Elke laag is in feite een filter dat invoergegevens verwerkt en specifieke kenmerken van objecten extraheert.
+
+            string connetionString;
+            SqlConnection cnn;
+            connetionString = @"Server=localhost;Database=master;Trusted_Connection=True;";
+            cnn = new SqlConnection(connetionString);
+            cnn.Open();
+            MessageDialog dialog = new MessageDialog("text");
+            cnn.Close();
+        }
+
         private void bin_click(object sender, RoutedEventArgs e)
         {
             /* Here are all the variables that we are going to need to use */
@@ -303,8 +422,10 @@ namespace Rekenmachine
                 textbox1.Text = ("Syntax Error");
 
             }
-            else
+            //alleen uitvoeren als het textvak gevuld is EN als het aantal getallen niet meer is dan 3 EN dat de textbox niet gevuld is met iets anders dan een INT
+            else if (textbox1.Text != "" && textbox1.Text.Length <= 3 && CheckSymbol(textbox1.Text))
             {
+                //
                 n = Convert.ToInt32(textbox1.Text);
                 textbox2.Text = n + " =";
 
@@ -313,14 +434,14 @@ namespace Rekenmachine
                     n *= -1;
                 }
 
-                /* This for-loop divides the number in the TextBoxDisplay to check how long the binary number is and to check where there has to be a '1' or a '0'*/
+                /* Deze  FOR-loop verdeelt het nummer in het TextBoxDisplay om te controleren hoe lang het binaire getal is en om te controleren waar er een '1' of een '0' moet zijn */
                 for (i = 0; n > 0; i++)
                 {
                     a[i] = n % 2; // 1
                     n /= 2;
                 }
 
-                /* This for-loop grabs the data from the array and stores it in the String 'finalResult' */
+                /* Deze FOR-loop gebruikt de date vanaf de array en stores het in de strings it 'finalResult' */
                 for (i -= 1; i >= 0; i--)
                 {
                     finalResult += "" + a[i];
@@ -330,29 +451,15 @@ namespace Rekenmachine
                 textbox1.Text = finalResult;
 
             }
-        }
 
-        //textbox management that shows sql database history of previously inputted values to the calculator
-        private void history_click(object sender, RoutedEventArgs e)
-        {
-            if (textbox2.Visibility == Visibility.Visible)
-            {
-                textbox2.Visibility = Visibility.Collapsed;
-            }
-            else if (textbox2.Visibility == Visibility.Collapsed)
-            {
-                textbox2.Visibility = Visibility.Visible;
-            }
-        }
-
-        private void textbox1_TextChanged(object sender, TextChangedEventArgs e)
-        {
 
         }
+
+
 
         private void textbox2_TextChanged(object sender, TextChangedEventArgs e)
         {
-            double newVar = getal1 + getal2 + result;
+            double newVar = result;
             textbox2.Text = newVar.ToString();
         }
     }
